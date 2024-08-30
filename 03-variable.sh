6748ab9765845e9d3220c433f3931acbebc0eedc
venkat <venkat.durga60@gmail.com>
2024-08-30 19:31:42 +0530

    person variable

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=RAJU
+PERSON2=RAVI
+PERSON3=JoinDevOps
+
+echo "${PERSON1}:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: I am doing well ${PERSON1}, What about you?"
+echo "${PERSON1}:: I am good what are you doing ${PERSON2}?"
+echo "${PERSON2}:: I am learning devops in ${PERSON3}"
+echo "${PERSON3}:: welcome to ${PERSON3}"
