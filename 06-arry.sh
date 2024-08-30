e100befb16acdd13f26d045bc717c899a1feac12
venkat <venkat.durga60@gmail.com>
2024-08-31 03:10:23 +0530

    array variable

--- 06-arry.sh
+++ 06-arry.sh
@@ -5,4 +5,4 @@ FRUITS=("APPLE" "ORANGE" "GRAPE")
 echo "first fruite is :: ${FRUITS[0]}"
 echo "second fruite is :: ${FRUITS[1]}"
 echo "third fruite is :: ${FRUITS[2]}"
-echo "all fruits is :: ${FRUITS[@]}
+echo "all fruits is :: ${FRUITS[@]}"
