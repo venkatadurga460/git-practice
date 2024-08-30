338419fd92468d608500a77d5a49d0278a06acbe
venkat <venkat.durga60@gmail.com>
2024-08-31 03:07:31 +0530

    arrya string

---
+++ 06-arry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "ORANGE" "GRAPE")
+
+echo "first fruite is :: ${FRUITS[0]}"
+echo "second fruite is :: ${FRUITS[1]}"
+echo "third fruite is :: ${FRUITS[2]}"
+echo "all fruits is :: ${FRUITS[@]}
