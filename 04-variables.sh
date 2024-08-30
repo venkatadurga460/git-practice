c0befeb6d7394b88538073a6d1907967d23e11f5
venkat <venkat.durga60@gmail.com>
2024-08-30 19:44:50 +0530

    variables with manual call

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "${PERSON1}:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: I am doing well ${PERSON1}, What about you?"
+echo "${PERSON1}:: I am good what are you doing ${PERSON2}?"
+echo "${PERSON2}:: I am learning devops in ${PERSON3}"
+echo "${PERSON3}:: welcome to ${PERSON3}"
