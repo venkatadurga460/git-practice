89a14f4bd0dc9f0f5cc468283f3decc28b6f76cf
venkat <venkat.durga60@gmail.com>
2024-08-31 05:33:35 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,9 @@
 
 Number=$1
 
-if [ $Number gt 20 ]
+if [ $Number -gt 20 ]
 then
 echo "given number :: $Number is greterthan 20"
 else
-echo "given number :: $Number is lees  than 20"
+echo "given number :: $Number is leesthan 20"
 fi
