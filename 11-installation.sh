1a847aa6af3048c5f0f3c4a30285844ddc1de3d5
venkat <venkat.durga60@gmail.com>
2024-08-31 06:17:06 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,3 +10,12 @@ exit 1
 else
 echo "you have root access please procedd further"
 fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+echo "Git is not installed please go head and install"
+else 
+echo "Git is installed, no need to perform"
+fi
